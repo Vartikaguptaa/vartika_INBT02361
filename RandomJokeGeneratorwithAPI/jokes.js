b4c89abed04718c7ffe01ba9f28aa5ed5e82c2e1
Vartika Gupta <vartika6100@gmail.com>
2023-08-14 21:13:32 +0530

    Update jokes.js

--- RandomJokeGeneratorwithAPI/jokes.js
+++ RandomJokeGeneratorwithAPI/jokes.js
@@ -1,24 +1,14 @@
 window.onload= function()
 {
-    var fun= document.getElementById("Fun_btn")
     var dark= document.getElementById("Dark_btn")
     var spooky= document.getElementById("Spooky_btn")
-    var random_joke= document.getElementById("Random_btn")
     var prog= document.getElementById("Prog_btn")
     var misc= document.getElementById("Miscs_btn")
     
-    fun.addEventListener("click",submit_fun)
     dark.addEventListener("click",submit_dark)
     spooky.addEventListener("click",submit_spooky)
-    random_joke.addEventListener("click", submit_random)
     prog.addEventListener("click", submit_prog)
     misc.addEventListener("click", submit_miscs)
-}
-
-function submit_fun()
-{
-    var fun_value= document.getElementById("Fun_btn").value
-    joke_req(fun_value)
 }
 
 function submit_dark()
@@ -31,12 +21,6 @@ function submit_spooky()
 {
     var spooky_value= document.getElementById("Spooky_btn").value
     joke_req(spooky_value)
-}
-
-function submit_random()
-{
-    var random_joke_value= document.getElementById("Random_btn").value
-    joke_req(random_joke_value)
 }
 
 function submit_prog()
